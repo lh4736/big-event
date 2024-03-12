b8ffa3d4d445f9cb0195e17e14ab3c8d7ca3bc6e
lh <473656561@qq.com>
2024-03-12 19:24:32 +0800

    fix:测试

--- src/main/java/com/example/bigevent/BigEventApplication.java
+++ src/main/java/com/example/bigevent/BigEventApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BigEventApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BigEventApplication.class, args);
     }
 
